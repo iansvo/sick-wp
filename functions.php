bd674cfed9a51eafea6db2821377f75070c5e203
Ian Svoboda <ian@iansvoboda.com>
2022-03-11 13:09:56 -0500

    Fix typo in path
    I forgot to remove the /inc from the original path.

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 // Update to get_stylesheet_directory() if you're using a child theme
 $inc_directory = get_template_directory() . '/inc';
 
-require_once "$inc_directory/inc/cleanup.php"; 
-require_once "$inc_directory/inc/enqueues.php"; 
-require_once "$inc_directory/inc/widgets.php"; 
-require_once "$inc_directory/inc/menus.php"; 
+require_once "$inc_directory/cleanup.php"; 
+require_once "$inc_directory/enqueues.php"; 
+require_once "$inc_directory/widgets.php"; 
+require_once "$inc_directory/menus.php"; 
 
 // Add new requires below
